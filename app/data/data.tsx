664c3f3d53c58274d1c3d12bcb8b5e9bde85cee9
Ismail <iso_yaman@outlook.com>
2023-07-06 02:17:30 +0200

    Added data for the protfolio.

--- app/data/data.tsx
+++ app/data/data.tsx
@@ -217,8 +217,243 @@ export const projects = [
     {
         id: 1,
         title: "Achromira",
-        description: "Achromira is a story rich platformer where Mira, the character you play as, has to search for all the crystal shards in order to restore colour to her own world and that of others. Follow her on her journey to rescue three unique worlds, Achrome City, Rutate and Gryd. This was the second project with the game development minor. As the artist in the team, I've worked mainly on the first level of the game. Almost all the assets in this level are done by me.",
+        description: [
+            {
+                body1: "Achromira is a story rich platformer where Mira, the character you play as, has to search for all the crystal shards in order to restore colour to her own world and that of others. Follow her on her journey to rescue three unique worlds, Achrome City, Rutate and Gryd.",
+            },
+            {
+                body2: "This was the second project with the game development minor. As an artist within the team, I've worked mainly on the first level of the game. Almost all the assets in this level are done by me. Next to that I've also worked on various assets in other levels.",
+            },
+        ],
         image: "https://i.imgur.com/5QZ3Z9M.png",
+        links: [
+            {
+                label: "Visit website",
+                url: "https://project.cmi.hr.nl/2021_2022/game_greenhouse/",
+            },
+            {
+                label: "View on Steam",
+                url: "https://store.steampowered.com/app/1809760/Achromira/",
+            },
+        ],
+        tools: [
+            {
+                label: "Unity",
+                image: "https://img.icons8.com/ios-filled/50/000000/unity.png",
+            },
+            {
+                label: "Blender",
+                image: "https://img.icons8.com/color/48/000000/blender-3d.png",
+            },
+            {
+                label: "Adobe Photoshop",
+                image: "https://img.icons8.com/color/48/000000/adobe-photoshop--v1.png",
+            },
+            {
+                label: "Adobe Illustrator",
+                image: "https://img.icons8.com/color/48/000000/adobe-illustrator--v1.png",
+            },
+        ],
     },
+    {
+        id: 2,
+        title: "Shell Shock",
+        description: [
+            {
+                body1: "A 2D mobile game that we made in 6 weeks at the game development minor. Shell Shock is a simple 2D platformer where you need to defeat your enemies.",
+            },
+            {
+                body2: "As the artist in the team, I've worked on the graphics of the game. Most of the sprites and animations are made by me. This was my first experience of making 2D pixel-art.",
+
+            }
+        ],
+        image: "https://i.imgur.com/5QZ3Z9M.png",
+        links: [
+            {
+                label: "Check on Play Store",
+                url: "https://play.google.com/store/apps/details?id=com.SingletonStudio.ShellShock",
+            },
+        ],
+        tools: [
+            {
+                label: "Unity",
+                image: "https://img.icons8.com/ios-filled/50/000000/unity.png",
+            },
+            {
+                label: "Aseprite",
+                image: "https://img.icons8.com/color/48/000000/aseprite.png",
+            },
+            {
+                label: "Adobe Photoshop",
+                image: "https://img.icons8.com/color/48/000000/adobe-photoshop--v1.png",
+            },
+        ],
+
+                
+    },
+    {
+        id: 3,
+        title: "Waar Bekken Ik?",
+        description: [
+            {
+                body1: "This was another schoolproject where I and my team had to solve a problem that medical students at the Erasmus University were facing. The pelvis is one of the few parts of the human body that is very difficult for students to understand only from books. We made a 3D visualization that makes the pelvis clear to students by showing it from different perspectives.",
+            },
+            {
+                body2: "This was a long project that lasted six months. During that time, we worked with the scrum and agile principles. In addition, the special thing about this project is that it is a long-term project. Hopefully next year's CMGT students will continue with it.",
+            },
+        ],
+        image: "https://i.imgur.com/5QZ3Z9M.png",
+        links: [
+            {
+                label: "Read more",
+                url: "https://cmgt.hr.nl/projecten/waar-bekken-ik",
+            },
+        ],
+        tools: [
+            {
+                label: "React",
+                image: "https://img.icons8.com/color/48/000000/react-native.png",
+            },
+            {   
+                label: "Scrum",
+                image: "https://img.icons8.com/color/48/000000/scrum.png",
+            },
+            {
+                label: "Blender",
+                image: "https://img.icons8.com/color/48/000000/blender-3d.png",
+            },
+        ],
+    },
+    {
+        id: 4,
+        title: "Akropolis",
+        description: [
+            {
+                body1:"This was a schoolproject where I had to find my own client to make a website for. At the time I worked at a Greek restaurant and the owner needed a new website. Now I could combine my school with work and learn new things in the process."
+            },
+        ],
+        image: "https://i.imgur.com/5QZ3Z9M.png",
+        links: [
+            {
+                label: "Visit website",
+                url: "https://ismailkayadelen.com/akropolis/reserveringssysteem/",
+            },
+        ],
+        tools: [
+            {
+                label: "PHP",
+                image: "https://img.icons8.com/officel/48/000000/php-logo.png",
+            },
+            {
+                label: "MySQL",
+                image: "https://img.icons8.com/ios-filled/50/000000/mysql-logo.png",
+            },
+            {
+                label: "Bootstrap",
+                image: "https://img.icons8.com/color/48/000000/bootstrap.png",
+            },
+        ],
+    },
+    {
+        id: 5,
+        title: "Unreal Engine 4 Shooter",
+        description: [
+            {
+                body1: "This was a schoolproject where I had to specialize in a certain program, language, IOT, etc. I chose to make a game with the Unreal Engine 4.",
+            },
+        ],
+        image: "https://i.imgur.com/5QZ3Z9M.png",
+        links: [
+            {
+                label: "View demo on YouTube",
+                url: "https://www.youtube.com/playlist?list=PLlibq_pnekYPDebVB2VMDJfsEDNbRWXOF",
+            },
+        ],
+        tools: [
+            {
+                label: "Unreal Engine 4",
+                image: "https://img.icons8.com/color/48/000000/unreal-engine.png",
+            },
+        ],
+    },
+    {
+        id: 6,
+        title: "School blog",
+        description: [
+            {
+                body1: "The first asignment when I started at CMGT was to make a personal blog in which I could document all the tasks I've done.",
+            },
+        ],
+        image: "https://i.imgur.com/5QZ3Z9M.png",
+        links: [
+            {
+                label: "Visit blog",
+                url: "https://stud.hosted.hr.nl/0964864/",
+            },
+        ],
+        tools: [
+            {
+                label: "WordPress",
+                image: "https://img.icons8.com/color/48/000000/wordpress.png",
+            },
+        ],
+    },
+    {
+        id: 7,
+        title: "SSI Rotterdam",
+        description: [
+            {
+                body1: "SSI (Seksuele straatintimidatie) Rotterdam was a project for the municipality of Rotterdam aimed at raising awareness about sexual street harassment in busy areas of the city. It involved a VR experience that put users in the shoes of victims of street harassment. The goal was to foster empathy and initiate a dialogue about this issue, ultimately leading to increased awareness and positive change in society.",
+            },
+        ],
+        image: "https://i.imgur.com/5QZ3Z9M.png",
+        links: [
+            {
+                label: "Read more",
+                url: "http://project.hosted.hr.nl/2021_2022/mlab2_strin_t6/",
+            },
+        ],
+        tools: [
+            {
+                label: "Unity",
+                image: "https://img.icons8.com/ios-filled/50/000000/unity.png",
+            },
+        ],
+    },
+    {
+        id: 8,
+        title: "SkeeballVK",
+        description: [
+            {
+                body1: "This was a schoolproject where I had to make a landingpage for a product that we had designed. The website was targeted towards parents that wanted an educational toy for their young kids. Hence the playful looks of the website.",
+            },
+        ],
+        image: "https://i.imgur.com/5QZ3Z9M.png",
+        links: [
+            {
+                label: "Visit website",
+                url: "https://stud.hosted.hr.nl/0964864/skeeballvk/",
+            },
+        ],
+        tools: [
+            {
+                label: "HTML",
+                image: "https://img.icons8.com/color/48/000000/html-5.png",
+            },
+            {
+                label: "CSS",
+                image: "https://img.icons8.com/color/48/000000/css3.png",
+            },
+            {
+                label: "JavaScript",
+                image: "https://img.icons8.com/color/48/000000/javascript.png",
+            },
+        ],
+    },
+    
+
+
+
+
 
 ]
