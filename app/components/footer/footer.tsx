789ac323bd32f9c4c3064b47abc1176163c9f14d
IsmailYaman <iso_yaman@outlook.com>
2023-10-19 16:15:14 +0200

    Fixed footer background

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,17 +1,17 @@
 import SocialLinks from "./social";
 import { socials } from "../../data/data";
-import Image from "next/image";
 
 const Footer = () => {
     return (
-        <footer className="footer footer-center p-10 text-primary-content bg-[url('/img/oooscillate.svg')] bg-no-repeat bg-top bg-cover">
+        
+        <footer className="footer footer-center sm:h-screen md:h-96 p-10 text-primary-content bg-[url('/img/oooscillate.svg')] bg-no-repeat bg-center bg-cover ">
             <aside>
                 {/* <Image src={"/img/svg.svg"} fill className="" alt={"footer background"} /> */}
             </aside>
             <SocialLinks socials={socials} />
             <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
         </footer>
-
+        
         // <footer className="footer footer-center bg-no-repeat h-screen bg-cover bg-center p-10 text-base-content bg-[url('/img/oooscillate.svg')]">
         // {/* <footer className="footer footer-center text-base-content "> */}
         //     {/* <Image src={"/img/oooscillate.svg"} fill className="" alt={"footer background"} /> */}
