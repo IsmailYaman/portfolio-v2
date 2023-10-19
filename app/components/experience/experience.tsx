e62034cb2770986ae486d259f3bb59fb41166470
IsmailYaman <iso_yaman@outlook.com>
2023-10-19 15:51:21 +0200

    Fixed small responsiveness bug

--- app/components/experience/experience.tsx
+++ app/components/experience/experience.tsx
@@ -17,7 +17,7 @@ const Experiences = () => {
                 </div>
 
                 <h2>Ecommerce</h2>
-                <div className="flex flex-wrap items-center -mx-2">
+                <div className="flex flex-wrap items-center mx-2">
                     {experiences.ecommerce.map((ecommerce, index) => (
                         <div key={ecommerce.id} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 px-2 mb-4">
                             <ExperiencesCard label={ecommerce.label} image={ecommerce.image} id={0} />
@@ -26,7 +26,7 @@ const Experiences = () => {
                 </div>
 
                 <h2>Design</h2>
-                <div className="flex flex-wrap items-center -mx-2">
+                <div className="flex flex-wrap items-center mx-2">
                     {experiences.design.map((design, index) => (
                         <div key={design.id} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 px-2 mb-4">
                             <ExperiencesCard label={design.label} image={design.image} id={0} />
@@ -35,7 +35,7 @@ const Experiences = () => {
                 </div>
 
                 <h2>Game Development</h2>
-                <div className="flex flex-wrap items-center -mx-2">
+                <div className="flex flex-wrap items-center mx-2">
                     {experiences.gameDevelopment.map((gameDevelopment, index) => (
                         <div key={gameDevelopment.id} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 px-2 mb-4">
                             <ExperiencesCard label={gameDevelopment.label} image={gameDevelopment.image} id={0} />
@@ -44,7 +44,7 @@ const Experiences = () => {
                 </div>
 
                 <h2 className="text-center">Other</h2>
-                <div className="flex flex-wrap items-center -mx-2">
+                <div className="flex flex-wrap items-center mx-2">
                     {experiences.other.map((other, index) => (
                         <div key={other.id} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 px-2 mb-4">
                             <ExperiencesCard label={other.label} image={other.image} id={0} />
