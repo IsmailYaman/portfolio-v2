c1b3218114c2e5bc21b4b546f048ff2aa3162fb4
IsmailYaman <iso_yaman@outlook.com>
2023-10-31 16:16:09 +0100

    Changed grid layout for different screen sizes for the about me section

--- app/components/about/about.tsx
+++ app/components/about/about.tsx
@@ -4,38 +4,38 @@ import Image from "next/image";
 import AboutSkills from "./aboutSkills";
 
 const About = () => {
-  return (
-    <div className="py-36 bg-base-100">
-      <div className="sm:container mx-auto">
-        <div className="grid grid-cols-2 grid-rows-2">
-          <div className="row-span-2 relative hover:transform hover:scale-105 transition duration-100 ease-in-out group">
-            <Image
-              src="/img/purple.png"
-              alt="Ismail"
-              className="rounded-2xl mx-auto shadow-2xl shadow-primary/50 group-hover:shadow-primary ease-out duration-100"
-              width={500}
-              height={500}
-            />
-            <div className="chat chat-start absolute w-80 right-0 top-5">
-              <div className="chat-bubble w-full">
-                <strong className="bg-gradient-to-br from-pink-600 via-purple-700 to-blue-400 text-transparent bg-clip-text font-black text-3xl">
-                  {about.title}
-                </strong>
-              </div>
+    return (
+        <div className="py-36 bg-base-100">
+            <div className="sm:container mx-auto">
+                <div className="grid lg:grid-cols-2 lg:grid-rows-1 grid-rows-1 lg:gap-x-10">
+                    <div className="relative hover:transform hover:scale-105 transition duration-100 ease-in-out group sm:row-span-2">
+                        <Image
+                            src="/img/purple.png"
+                            alt="Ismail"
+                            className="rounded-2xl mx-auto shadow-2xl shadow-primary/50 group-hover:shadow-primary ease-out duration-100 lg:mb-0 mb-16"
+                            width={500}
+                            height={500}
+                        />
+                        <div className="chat chat-start absolute w-80 right-0 top-5">
+                            <div className="chat-bubble w-full">
+                                <strong className="bg-gradient-to-br from-pink-600 via-purple-700 to-blue-400 text-transparent bg-clip-text font-black text-3xl">
+                                    {about.title}
+                                </strong>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex items-center lg:col-start-2">
+                        <h2 className="font-medium">
+                            I'm a <AboutSkills /> {about.body1}
+                        </h2>
+                    </div>
+                    <p className="text-justify lg:col-start-2 flex items-center">
+                        {about.body2} <br /> <br /> {about.body3}
+                    </p>
+                </div>
             </div>
-          </div>
-          <div className="flex items-center">
-          <h2 className="col-start-2 font-medium">
-            I'm a <AboutSkills /> {about.body1}
-          </h2>
-          </div>
-          <p className="col-start-2 flex items-center text-justify">
-            {about.body2} <br /> <br /> {about.body3}
-          </p>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default About;
