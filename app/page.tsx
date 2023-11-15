e4f3f32952cbcbd2c91b70d4d26067534995b439
IsmailYaman <iso_yaman@outlook.com>
2023-11-15 09:54:59 +0100

    Remove unused component and function imports.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Banner from "./components/banner/banner";
 import Banner2 from "./components/banner/banner2";
 import About from "./components/about/about";
-import RandomCatImage from "./functions/randomCatImage";
 import Projects from "./components/projects/projects";
 import Experience from "./components/experience/experience";
 import BackToTopButton from "./components/backToTop";
