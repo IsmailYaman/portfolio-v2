f351da36528f83b08841f9c5dee2236aeeeef774
IsmailYaman <iso_yaman@outlook.com>
2024-09-29 23:39:38 +0200

    Projects update

--- app/data/data.tsx
+++ app/data/data.tsx
@@ -50,6 +50,65 @@ export const education = [
 export const projects = [
     {
         id: 1,
+        title: "Porta Da Vida",
+        description: [
+            {
+                body1: "Porta Da Vida is a nursing home located in Holtum - the Netherlands. They provide two services: daytime activities for seniors and an atelier for young dementia patients.",
+                body2: "The client wanted a split-design for the homepage that would make it easy for users to find the information they need.",
+            },
+        ],
+        image: "/projects/portadavida.png",
+        links: [
+            {
+                label: "Visit website",
+                url: "https:/portadavida.nl/",
+            },
+        ],
+        tools: [
+            {
+                label: "Framer",
+                image: "https://img.icons8.com/color/48/000000/framer.png",
+            },
+
+        ],
+    },
+    {
+        id: 2,
+        title: "Fashioncheque - Shopfinder",
+        description: [
+            {
+                body1: "This client wanted a integration in their website that would make it easy for users to find the nearest store that accepts Fashioncheque. ",
+                body2: "Their previous solution was outdated and did not meet the requirements of the current time. ",
+            },
+        ],
+        image: "/projects/shopfinder.png",
+        links: [
+            {
+                label: "Visit website",
+                url: "https://www.fashioncheque.com/nl-nl/shopfinder",
+            },
+        ],
+        tools: [
+            {
+                label: "React",
+                image: "https://img.icons8.com/color/48/000000/a-frame.png",
+            },
+            {
+                label: "Typescript",
+                image: "https://img.icons8.com/color/48/000000/8th-wall.png",
+            },
+            {
+                label: "Storybook",
+                image: "https://img.icons8.com/color/48/000000/blender-3d.png",
+            },
+            {
+                label: "Tailwind",
+                image: "https://img.icons8.com/color/48/000000/blender-3d.png",
+            },
+        ],
+    },
+    {
+        id: 3,
         title: "CP-AR",
         description: [
             {
@@ -57,7 +116,7 @@ export const projects = [
                 body2: "CP-AR was my thesis project that I had the opportunity to develop for my studies in Creative Media & Game Technologies. Extensive research was done on the areas where improvements could be made within heart attack emergencies in the Netherlands.",
             },
         ],
-        image: "/img/purple.png",
+        image: "/projects/cp-ar.png",
         links: [
             {
                 label: "Visit website (recommended for mobile)",
@@ -81,7 +140,7 @@ export const projects = [
         ],
     },
     {
-        id: 2,
+        id: 4,
         title: "Achromira",
         description: [
             {
@@ -89,11 +148,11 @@ export const projects = [
                 body2: "This was the second project with the game development minor. As an artist within the team, I've worked mainly on the first level of the game. Almost all the assets in this level are done by me. Next to that I've also worked on various assets in other levels.",
             },
         ],
-        image: "/img/purple.png",
+        image: "/projects/achromira.png",
         links: [
             {
                 label: "Visit website",
-                url: "https://project.cmi.hr.nl/2021_2022/game_greenhouse/",
+                url: "https://store.steampowered.com/app/1809760/Achromira/",
             },
         ],
         tools: [
@@ -105,10 +164,6 @@ export const projects = [
                 label: "Blender",
                 image: "https://img.icons8.com/color/48/000000/blender-3d.png",
             },
-            // {
-            //     label: "Photoshop",
-            //     image: "https://img.icons8.com/color/48/000000/adobe-photoshop--v1.png",
-            // },
             {
                 label: "Illustrator",
                 image: "https://img.icons8.com/color/48/000000/adobe-illustrator--v1.png",
@@ -116,7 +171,7 @@ export const projects = [
         ],
     },
     {
-        id: 3,
+        id: 5,
         title: "Waar Bekken Ik?",
         description: [
             {
@@ -124,7 +179,7 @@ export const projects = [
                 body2: "This was a long project that lasted six months. During that time, we worked with the scrum and agile principles. In addition, the special thing about this project is that it is a long-term project. Hopefully next year's CMGT students will continue with it.",
             },
         ],
-        image: "/img/purple.png",
+        image: "/projects/waarbekkenik.png",
         links: [
             {
                 label: "Read more",
@@ -133,112 +188,57 @@ export const projects = [
         ],
         tools: [
             {
-                label: "React",
+                label: "Unity",
                 image: "https://img.icons8.com/color/48/000000/react-native.png",
             },
             {
-                label: "Scrum",
-                image: "https://img.icons8.com/color/48/000000/scrum.png",
-            },
-            {
-                label: "Blender",
-                image: "https://img.icons8.com/color/48/000000/blender-3d.png",
-            },
-        ],
-    },
-    {
-        id: 4,
-        title: "Shooter Game",
-        description: [
-            {
-                body1: "This was a schoolproject where I had to specialize in a certain program, language, IOT, etc. I chose to make a game with the Unreal Engine 4.",
-            },
-        ],
-        image: "/img/purple.png",
-        links: [
-            {
-                label: "View demo ",
-                url: "https://www.youtube.com/playlist?list=PLlibq_pnekYPDebVB2VMDJfsEDNbRWXOF",
-            },
-        ],
-        tools: [
-            {
-                label: "Unreal Engine 4",
-                image: "https://img.icons8.com/color/48/000000/unreal-engine.png",
-            },
-        ],
-    },
-    {
-        id: 5,
-        title: "School blog",
-        description: [
-            {
-                body1: "The first asignment when I started at CMGT was to make a personal blog in which I could document all the tasks I've done.",
-            },
-        ],
-        image: "/img/purple.png",
-        links: [
-            {
-                label: "Visit blog",
-                url: "https://stud.hosted.hr.nl/0964864/",
-            },
-        ],
-        tools: [
-            {
-                label: "WordPress",
-                image: "https://img.icons8.com/color/48/000000/wordpress.png",
+                label: "React",
+                image: "https://img.icons8.com/color/48/000000/react-native.png",
             },
         ],
     },
     {
         id: 6,
-        title: "SSI Rotterdam",
+        title: "VIUW makelaars",
         description: [
             {
-                body1: "SSI (Seksuele straatintimidatie) Rotterdam was a project for the municipality of Rotterdam aimed at raising awareness about sexual street harassment in busy areas of the city. It involved a VR experience that put users in the shoes of victims of street harassment. The goal was to foster empathy and initiate a dialogue about this issue, ultimately leading to increased awareness and positive change in society.",
+                body1:"This client wanted a sleek and modern website that would make it easy for users to find the information they need. I've created this website that is easy to navigate and has a clean design. The website is fully responsive and works on all devices.",
+                body2: "They specialze in taxations, sales and purchases of homes in the Netherlands.",
             },
         ],
-        image: "/img/purple.png",
+        image: "/projects/viuw.png",
         links: [
             {
-                label: "Read more",
-                url: "http://project.hosted.hr.nl/2021_2022/mlab2_strin_t6/",
+                label: "Visit website",
+                url: "hhttps://www.viuwmakelaars.nl/",
             },
         ],
         tools: [
             {
-                label: "Unity",
-                image: "https://img.icons8.com/ios-filled/50/000000/unity.png",
-            },
+                label: "Framer",
+                image: "https://img.icons8.com/color/48/000000/react-native.png",
+            }
         ],
     },
     {
         id: 7,
-        title: "SkeeballVK",
+        title: "Raamdesign Discounter ",
         description: [
             {
-                body1: "This was a schoolproject where I had to make a landingpage for a product that we had designed. The website was targeted towards parents that wanted an educational toy for their young kids. Hence the playful looks of the website.",
+                body1: "This client specializes in custom pleated curtains. They wanted a shopify webshop with a modern, easy to navigate and has a clean design with an epmhasis on a yellow/gold and black/gray design. ",
             },
         ],
-        image: "/img/purple.png",
+        image: "/projects/raamdesign.png",
         links: [
             {
-                label: "Visit website",
-                url: "https://stud.hosted.hr.nl/0964864/skeeballvk/",
+                label: "Visit webshop",
+                url: "https://raamdesigndiscounter.nl/",
             },
         ],
         tools: [
             {
-                label: "HTML",
-                image: "https://img.icons8.com/color/48/000000/html-5.png",
-            },
-            {
-                label: "CSS",
-                image: "https://img.icons8.com/color/48/000000/css3.png",
-            },
-            {
-                label: "JavaScript",
-                image: "https://img.icons8.com/color/48/000000/javascript.png",
+                label: "Shopify",
+                image: "https://img.icons8.com/color/48/000000/react-native.png",
             },
         ],
     },
@@ -246,22 +246,6 @@ export const projects = [
 
 export const experiences = {
     programming: [
-        // {
-        //     id: 1,
-        //     label: "HTML5",
-        //     image: "/icons/html.svg",
-        //     svg: "",
-        // },
-        // {
-        //     id: 2,
-        //     label: "CSS3",
-        //     image: "/icons/css.svg",
-        // },
-        // {
-        //     id: 3,
-        //     label: "JavaScript",
-        //     image: "/icons/javascript.svg",
-        // },
         {
             id: 4,
             label: "TypeScript",
@@ -313,18 +297,6 @@ export const experiences = {
             image: "/icons/next.svg",
         },
     ],
-    // ecommerce: [
-    //     {
-    //         id: 1,
-    //         label: "Shopify",
-    //         image: "/icons/shopify.svg",
-    //     },
-    //     {
-    //         id: 2,
-    //         label: "Magento",
-    //         image: "/icons/magento.svg",
-    //     },
-    // ],
     design: [
         {
             id: 1,
@@ -357,18 +329,6 @@ export const experiences = {
             image: "/icons/figma.svg",
         },
     ],
-    // gameDevelopment: [
-    //     {
-    //         id: 1,
-    //         label: "Unity",
-    //         image: "/icons/unity.svg",
-    //     },
-    //     {
-    //         id: 2,
-    //         label: "Unreal Engine",
-    //         image: "/icons/ue4.svg",
-    //     },
-    // ],
     other: [
         {
             id: 1,
